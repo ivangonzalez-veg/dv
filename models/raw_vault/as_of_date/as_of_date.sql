c66d843904daa215b12fe258df92e4da5731c9f4
ivangonzalez@veg.vet <ivangonzalez@veg.vet>
2022-03-01 23:06:39 +0000

    saving files on 3/1/2022

--- models/raw_vault/as_of_date/as_of_date.sql
+++ models/raw_vault/as_of_date/as_of_date.sql
@@ -2,7 +2,7 @@
 
 {%- set datepart = "day" -%}
 {%- set start_date = "TO_DATE('2020/01/01', 'yyyy/mm/dd')" -%}
-{%- set end_date = "TO_DATE('2022/02/11', 'yyyy/mm/dd')" -%}
+{%- set end_date = "TO_DATE('2022/02/13', 'yyyy/mm/dd')" -%}
 
 WITH as_of_date AS (
     {{ dbt_utils.date_spine(datepart=datepart, 
