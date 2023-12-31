5112c9e92a98d38d11bc171822db0dd058ca927e
ivangonzalez@veg.vet <ivangonzalez@veg.vet>
2023-12-31 12:18:27 +0000

    BUXTON_PATIENTS

--- models/compare.sql
+++ models/compare.sql
@@ -1,102 +1,90 @@
-create or replace view VEG.REPORTING.BUXTON_CLIENTS_DOGBYTE_TEST(
+create or replace view VEG.REPORTING.BUXTON_PATIENTS(
+	PATIENTID,
 	CLIENT_ID,
-	ADDR_LINE1,
-	ADDR_LINE2,
-	CITY,
-	STATE,
-	ZIP_CODE,
-	FIRST_NAME,
-	LAST_NAME,
-	PHONE_NUMBER,
-	EMAIL_ADDRESS,
-	DECLINE_RDVM,
-	REFERRAL_SOURCE,
-	RDVM_ID,
-	RDVM,
+	BIRTH_DATE,
+	SEX,
+	SPECIES,
+	BREED,
+	WEIGHT,
+	WEIGHT_UNIT,
 	UPDATED_AT,
 	FILE_LOAD_DATE
 ) as
-WITH clients AS
-(select     distinct 
-	CUSTOMER_ID CLIENT_ID,
-	ADDRESS_LINE1 ADDR_LINE1,
-	ADDRESS_LINE2 ADDR_LINE2,
-	CITY,
-	STATE,
-	ZIPCODE ZIP_CODE,
-    
-	FIRST_NAME,
-	LAST_NAME,
-	PHONE_NUMBER,
-	EMAIL EMAIL_ADDRESS,
-	DECLINE_RDVM,
-    
-	REFERRAL_SOURCE
-from VEG.REPORTING.V_DWH_ORDER_ITEMS) --select c1.* from c1;
--- client and count rdvm records
-,R_LIST AS
-(
-select distinct       CUSTOMER_ID   CLIENT_ID,
-            COUNT(RDVM_ID) over (partition by CUSTOMER_ID) RDVM_CNT2
-    ,   CASE
-                    WHEN NVL(RDVM_CNT2,0) = 0 THEN 'NONE'
-                    WHEN NVL(RDVM_CNT2,0) = 1 THEN RDVM_ID::text
-                    WHEN NVL(RDVM_CNT2,0) > 1 THEN 'MULTIPLE'
-          END RDVM_ID2,
-          CASE
-                    WHEN NVL(RDVM_CNT2,0) = 0 THEN 'NONE'
-                    WHEN NVL(RDVM_CNT2,0) = 1 THEN RDVM_NAME
-                    WHEN NVL(RDVM_CNT2,0) > 1 THEN 'MULTIPLE'
-          END RDVM2
-         FROM     VEG_DWH.RAW_VAULT.SAT_VETSPIRE_CLIENTS_RDVM_DETAILS
-) 
---select c1.* from   c1 where c1.client_id in(72116450,80002101,    3037539);
---select * from r_list  where client_id in(72116450,80002101,    3037539);
-, output as (
-select distinct
-          NVL(RDVM_ID2,'NONE') RDVM_id ,
-          NVL(RDVM2,'NONE') RDVM ,c1.*
-FROM      clients C1
-LEFT JOIN R_LIST
-ON        C1.CLIENT_ID = R_LIST.CLIENT_ID
-) --select rdvm,* from output;
-    , unmask as (
-    select   nvl(CLIENT_ID::text, ' ')|| ' ' CLIENT_ID, 
-                nvl(ADDR_LINE1::text, ' ')|| ' ' ADDR_LINE1, 
-                nvl(ADDR_LINE2::text, ' ')|| ' ' ADDR_LINE2, 
-                nvl(CITY::text, ' ')|| ' ' CITY, 
-                nvl(STATE::text, ' ')|| ' ' STATE, 
-                nvl(ZIP_CODE::text, ' ')|| ' ' ZIP_CODE, 
-                nvl(FIRST_NAME::text, ' ')|| ' ' FIRST_NAME, 
-                nvl(LAST_NAME::text, ' ')|| ' ' LAST_NAME, 
-                nvl(PHONE_NUMBER::text, ' ')|| ' ' PHONE_NUMBER, 
-                nvl(EMAIL_ADDRESS::text, ' ')|| ' ' EMAIL_ADDRESS, 
-                nvl(DECLINE_RDVM::text, ' ')|| ' ' DECLINE_RDVM, 
-                nvl(REFERRAL_SOURCE::text, ' ')|| ' ' REFERRAL_SOURCE, 
-                nvl(RDVM_ID::text, ' ')|| ' ' RDVM_ID, 
-                nvl(RDVM::text, ' ')|| ' ' RDVM, 
+
+with c as (select trim(patient_id)::text pid from VEG.REPORTING.BUXTON_CASES_REV where patient_id is not null group by patient_id)
+, source_data as (
+            SELECT     PATIENT_ID  PATIENTID,
+                       customer_id CLIENT_ID,
+                       BIRTH_DATE,
+                       SEX,
+                       SPECIES,
+                       BREED,
+                       WEIGHT_VALUE WEIGHT,
+                       WEIGHT_UNIT,
+                       --EFFECTIVE_DATE UPDATED_AT,
+                       convert_timezone('UTC','America/New_York', SYSDATE()) FILE_LOAD_DATE
+            FROM       veg_dwh.information_mart_stage.dim_patient_stage b
+            INNER JOIN c
+            ON         c.pid = trim(b.PATIENT_ID)::text
+)
+select nvl(a.PATIENTID::text, ' ')|| ' ' PATIENTID, 
+        nvl(a.CLIENT_ID::text, ' ')|| ' ' CLIENT_ID, 
+        nvl(a.BIRTH_DATE::text, ' ')|| ' ' BIRTH_DATE, 
+        nvl(a.SEX::text, ' ')|| ' ' SEX, 
+        nvl(a.SPECIES::text, ' ')|| ' ' SPECIES, 
+        nvl(a.BREED::text, ' ')|| ' ' BREED, 
+        nvl(a.WEIGHT::text, ' ')|| ' ' WEIGHT, 
+        nvl(a.WEIGHT_UNIT::text, ' ')||' ' WEIGHT_UNIT, 
             convert_timezone('UTC','America/New_York', sysdate()) Updated_at,
             convert_timezone('UTC','America/New_York', sysdate()) file_load_date
-        from output
-        union
-         select   	nvl(CLIENT_ID::text, ' ')|| ' ' CLIENT_ID, 
-                nvl(ADDR_LINE1::text, ' ')|| ' ' ADDR_LINE1, 
-                nvl(ADDR_LINE2::text, ' ')|| ' ' ADDR_LINE2, 
-                nvl(CITY::text, ' ')|| ' ' CITY, 
-                nvl(STATE::text, ' ')|| ' ' STATE, 
-                nvl(ZIP_CODE::text, ' ')|| ' ' ZIP_CODE, 
-                nvl(FIRST_NAME::text, ' ')|| ' ' FIRST_NAME, 
-                nvl(LAST_NAME::text, ' ')|| ' ' LAST_NAME, 
-                nvl(PHONE_NUMBER::text, ' ')|| ' ' PHONE_NUMBER, 
-                nvl(EMAIL_ADDRESS::text, ' ')|| ' ' EMAIL_ADDRESS, 
-                nvl(DECLINE_RDVM::text, ' ')|| ' ' DECLINE_RDVM, 
-                nvl(REFERRAL_SOURCE::text, ' ')|| ' ' REFERRAL_SOURCE, 
-                nvl(RDVM_ID::text, ' ')|| ' ' RDVM_ID, 
-                nvl(RDVM::text, ' ')|| ' ' RDVM, 
-                	UPDATED_AT,
-                	FILE_LOAD_DATE
-        from veg_dwh.bv.dogbyte_buxton_clients
+ from source_data a
+ 
+/* 
+WITH p1 AS
+(SELECT  "Patient ID"  patient_id
+         FROM     "VEG"."REPORTING"."VW_SALES_DETAIL_DWH"
+         WHERE    "Date of Invoice (Local)" BETWEEN '2020-01-01 00:00:00.000' AND sysdate() and "Invoice Status" in ('Paid','Due')group by "Patient ID")
+, source_data as (
+SELECT     p.id patientid,
+           p.client_id,
+           p.birth_date,
+           p.sex,
+           p.species,
+           p.breed,
+           p.weight,
+           p.weight_unit
+FROM       "PRESENTATION_DB"."PRESENTATION"."TBL_PATIENTS" p
+INNER JOIN p1
+ON         p1.patient_id = p.id)
 
-        
-        )
-        select * from unmask;
+select nvl(a.PATIENTID::text, ' ')|| ' ' PATIENTID, 
+        nvl(a.CLIENT_ID::text, ' ')|| ' ' CLIENT_ID, 
+        nvl(a.BIRTH_DATE::text, ' ')|| ' ' BIRTH_DATE, 
+        nvl(a.SEX::text, ' ')|| ' ' SEX, 
+        nvl(a.SPECIES::text, ' ')|| ' ' SPECIES, 
+        nvl(a.BREED::text, ' ')|| ' ' BREED, 
+        nvl(a.WEIGHT::text, ' ')|| ' ' WEIGHT, 
+        nvl(a.WEIGHT_UNIT::text, ' ')||' ' WEIGHT_UNIT, 
+            convert_timezone('UTC','America/New_York', sysdate()) Updated_at,
+            convert_timezone('UTC','America/New_York', sysdate()) file_load_date
+ from source_data a
+ inner join VEG.REPORTING.BUXTON_CLIENTS c
+    on a.client_id = c.client_id
+    */
+    /*
+where A.client_id in( 49582329, 
+75287552, 
+56138957, 
+64660282, 
+80002101, 
+71299268, 
+72358756, 
+77470900, 
+71045416, 
+71145029, 
+72096980, 
+16694909, 
+67413615, 
+81027116, 
+3583049)*/
+;
